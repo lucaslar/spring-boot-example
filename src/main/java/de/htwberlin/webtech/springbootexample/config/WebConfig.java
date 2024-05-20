8e6b69ef53f851d079e4825ceb7c378c388c1e27
Lucas Larisch <lucas.larisch@googlemail.com>
2024-05-20 16:24:11 +0200

    Add heroes FE to allowed origins.

--- src/main/java/de/htwberlin/webtech/springbootexample/config/WebConfig.java
+++ src/main/java/de/htwberlin/webtech/springbootexample/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry
                 .addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:5173");
+                .allowedOrigins("http://localhost:5173", "https://heroes-frontend.onrender.com");
     }
 }
