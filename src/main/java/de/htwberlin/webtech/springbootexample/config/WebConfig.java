d0e2249d65193982de9ad2d14f8d13edb6560787
Lucas Larisch <lucas.larisch@googlemail.com>
2024-05-20 14:56:57 +0200

    Added missing annotations for cors.

--- src/main/java/de/htwberlin/webtech/springbootexample/config/WebConfig.java
+++ src/main/java/de/htwberlin/webtech/springbootexample/config/WebConfig.java
@@ -1,8 +1,12 @@
 package de.htwberlin.webtech.springbootexample.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
